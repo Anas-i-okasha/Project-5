0e7bb71e2d80930d331866722ce75e56047a7a6c
Anas-i-okasha <okasha.i.anas@gmail.com>
2020-11-26 19:34:25 +0200

    fronend/get

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import axios from 'axios'
+import {useState} from 'react'
 import './App.css';
 
-const getAllArticles=()=>{
- console.log('i want to get all articles')
-}
+
 const App = () => {
+  const [articles , setarticles] = useState([])
+
+  const getAllArticles=()=>{
+    axios.get('http://localhost:5000/articles')
+    .then((responce)=>{
+      console.log('Responce', responce)
+      setarticles(responce.data)
+    })
+    .catch((err)=>{
+      console.log('Err' , err)
+    })
+   }
+
   return (
     <div>
-      <h1>HELLO WORLD</h1>
+      <h1>PROJECT - 4</h1>
 
-      <div> <button onClick={getAllArticles}>Get all articles</button> </div>
+      <div className='btn'> <button onClick={getAllArticles}>Get all articles</button> 
+
+      {articles.map((elem)=>{
+       return  <div className='get'> <span> Title: {elem.title}</span> <span>Description: {elem.description}</span> <span> Author: {elem.author} </span> </div>
+      })}
+
+      </div>
     </div>
   );
 };
